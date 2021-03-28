c178643785a3828c59e72d59d658468bea098c9c
dnldn <81538598+dnldn@users.noreply.github.com>
2021-03-28 16:42:41 -0700

    Add files via upload
    Compiled batch programs for reaper using the Aubio repository. Need to be placed in reaper resourcepath/userplugins.
    https://github.com/aubio/aubio
    https://aubio.org/

---
+++ aubio/0.4.9/win64/onset.bat
@@ -0,0 +1,3 @@
+@echo off 
+aubioonset A:\Active Sessions (Reaper)\Script testing\DNLDN aubio onset transient slicer\Audio files\syn1.wav > c:\Users\Studio\AppData\Roaming\REAPER\UserPlugins\aubio\0.4.9\win64\syn1.txt
+echo 
